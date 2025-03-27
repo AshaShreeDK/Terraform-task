fd0004e2ab7dc0425c1919ada2c758f761be907c
AshaShreeDK <dlrashadk@gmail.com>
2025-03-28 00:01:40 +0530

    Update main.tf

--- Task12/main.tf
+++ Task12/main.tf
@@ -4,7 +4,7 @@ data "aws_vpc" "myvpc" {
 
 data "aws_availability_zones" "az_zones" {}
 
-data "aws_subnet" "selected" {
+data "aws_subnet" "subnet" {
   count = 3
 
   filter {
@@ -22,12 +22,11 @@ resource "aws_instance" "servers" {
   count                       = 3
   ami                         = var.aws_ami_id
   instance_type               = var.aws_instance_type
-  subnet_id                   = data.aws_subnet.selected[count.index].id
+  subnet_id                   = data.aws_subnet.subnet[count.index].id
   associate_public_ip_address = true
   key_name                    = var.key_name
 
   tags = {
-          Name = "task12-server1${substr(element(data.aws_availability_zones.az_zones.names, count.index), -1, 1)}"
+    Name = "task12-server1${substr(element(data.aws_availability_zones.az_zones.names, count.index), -1, 1)}"
   }
 }
-
