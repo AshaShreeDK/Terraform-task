8997f6d4aa472522fff3049fc838b944c3e33abd
AshaShreeDK <dlrashadk@gmail.comcom>
2025-03-27 06:56:50 +0000

    Initial commit: Terraform code

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "server_public_ip" {
+  value = aws_instance.task11_server1a.public_ip
+}
+
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_ami_id" {
+  type = string
+}
+
+variable "aws_instance_type" {
+  type = string
+}
+
+variable "server_name" {
+  type = string
+}
+
