8e2c7476706a82c3c383e2fb9e46c7685da9c5fc
AshaShreeDK <dlrashadk@gmail.com>
2025-03-31 14:19:14 +0530

    Create variables.tf

--- task13/variables.tf
+++ task13/variables.tf
@@ -1,15 +1,22 @@
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
 variable "aws_ami_id" {
   type = string
 }
 
 variable "aws_instance_type" {
-  type = string
+  type    = string
+  default = "t3.micro"
 }
 
 variable "key_name" {
   type = string
 }
 
-variable "server_name" {
-  type = string
+variable "lb_name" {
+  type    = string
+  default = "task13-loadbalancer"
 }
