672e01e59e50dc841b6e2af152f5232f79e255c8
AshaShreeDK <dlrashadk@gmail.com>
2025-03-31 14:16:38 +0530

    create outputs.tf

--- task13/outputs.tf
+++ task13/outputs.tf
@@ -1,3 +1,20 @@
-output "server_public_ips" {
-  value = aws_instance.servers[*].public_ip
+output "vpc_id" {
+  value = aws_vpc.task13_vpc.id
 }
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "app_subnets" {
+  value = aws_subnet.app[*].id
+}
+
+output "db_subnets" {
+  value = aws_subnet.db[*].id
+}
+
+output "load_balancer_dns" {
+  value = aws_lb.task13_lb.dns_name
+}
+
