07be92567b5924c411676555245aff4039de577f
AshaShreeDK <dlrashadk@gmai.com>
2025-03-31 08:15:46 +0000

    Your commit message

--- main.tf
+++ main.tf
@@ -1,26 +1,59 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet" "subnet_1a" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-  filter {
-    name   = "availability-zone"
-    values = ["eu-north-1a"]
-  }
-}
-
-resource "aws_instance" "task11_server1a" {
-  ami                         = var.aws_ami_id
-  instance_type               = var.aws_instance_type
-  subnet_id                   = data.aws_subnet.subnet_1a.id
-  associate_public_ip_address = true
+resource "aws_vpc" "task13_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = var.server_name
+    Name = "task13-vpc"
+  }
+}
+
+data "aws_availability_zones" "az_zones" {}
+
+resource "aws_subnet" "public" {
+  count             = 3
+  vpc_id            = aws_vpc.task13_vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index)
+  availability_zone = element(data.aws_availability_zones.az_zones.names, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "task13-subnet-public-${substr(element(data.aws_availability_zones.az_zones.names, count.index), -1, 1)}"
+  }
+}
+
+resource "aws_subnet" "app" {
+  count             = 3
+  vpc_id            = aws_vpc.task13_vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index + 3)
+  availability_zone = element(data.aws_availability_zones.az_zones.names, count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "task13-subnet-app-${substr(element(data.aws_availability_zones.az_zones.names, count.index), -1, 1)}"
+  }
+}
+
+resource "aws_subnet" "db" {
+  count             = 3
+  vpc_id            = aws_vpc.task13_vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index + 6)
+  availability_zone = element(data.aws_availability_zones.az_zones.names, count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "task13-subnet-db-${substr(element(data.aws_availability_zones.az_zones.names, count.index), -1, 1)}"
+  }
+}
+
+resource "aws_lb" "task13_lb" {
+  name               = var.lb_name
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public[*].id
+
+  tags = {
+    Name = var.lb_name
   }
 }
 
--- samplevars.tfvars
+++ samplevars.tfvars
@@ -1,4 +1,5 @@
 aws_ami_id        = "ami-0f65a9eac3c203b54"
 aws_instance_type = "t3.micro"
-server_name       = "task11-server1a"
+key_name          = "Asha-Europe"
+
 
