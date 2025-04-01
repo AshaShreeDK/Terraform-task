cf49e63e8d3a0d780d8a777369a67a1d7479f3a7
AshaShreeDK <dlrashadk@gmail.com>
2025-04-02 02:06:17 +0530

    Update variables.tf

--- task13/variables.tf
+++ task13/variables.tf
@@ -14,3 +14,20 @@ variable "subnet_count" {
   type = number
 }
 
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "subnet_cidrs" {
+  type = map(list(string))
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "subnet_types" {
+  type    = list(string)
+}
+
+
